a8f22c56835e7e3473fd2e8617c6f052846a2da1
ASDomoroschenov <sashachicha228@mail.ru>
2024-08-12 21:26:09 +0300

    finish

--- JavaAvlTree/src/main/java/ru/mai/Main.java
+++ JavaAvlTree/src/main/java/ru/mai/Main.java
@@ -11,8 +11,6 @@ import ru.mai.associative_container.impl.AvlTreeFactor;
 import ru.mai.associative_container.impl.AvlTreeHeight;
 
 import javax.swing.*;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Random;
 
 public class Main {
@@ -23,39 +21,10 @@ public class Main {
 
 
     public static void main(String[] args) {
-//        testAvlTree();
-        perfomanceCompareTest();
+        performanceCompareTest();
     }
 
-    public static void testAvlTree() {
-        for (int i = 0; i < 1000; i++) {
-            AssociativeContainer<Integer, Integer> avlTree = new AvlTreeHeight<>();
-            List<Integer> keys = new ArrayList<>();
-
-            try {
-                for (int j = 0; j < 100000; j++) {
-                    int key = getRandomKey();
-
-                    if (avlTree.insert(key, 1)) {
-                        keys.add(key);
-                    }
-                }
-
-                for (int j = 0; j < keys.size(); j++) {
-                    avlTree.delete(keys.get(j));
-                }
-            } catch (Exception ex) {
-                System.out.println(ex.getMessage());
-                System.out.println(keys);
-            }
-        }
-    }
-
-    private static int getRandomKey() {
-        return random.nextInt(100);
-    }
-
-    private static void perfomanceCompareTest() {
+    private static void performanceCompareTest() {
         XYSeries heightInsertSeries = new XYSeries("AvlTreeHeight - Insert");
         XYSeries factorInsertSeries = new XYSeries("AvlTreeFactor - Insert");
 
@@ -97,10 +66,10 @@ public class Main {
             factorDeleteSeries.add(numOperations, factorDeleteTime);
         }
 
-        displayChart("Insert Operation Performance", "Number of Operations", "Time (ns)", heightInsertSeries, factorInsertSeries);
-        displayChart("Find Operation Performance", "Number of Operations", "Time (ns)", heightFindSeries, factorFindSeries);
-        displayChart("Update Operation Performance", "Number of Operations", "Time (ns)", heightUpdateSeries, factorUpdateSeries);
-        displayChart("Delete Operation Performance", "Number of Operations", "Time (ns)", heightDeleteSeries, factorDeleteSeries);
+        displayChart("Insert Operation Performance", heightInsertSeries, factorInsertSeries);
+        displayChart("Find Operation Performance", heightFindSeries, factorFindSeries);
+        displayChart("Update Operation Performance", heightUpdateSeries, factorUpdateSeries);
+        displayChart("Delete Operation Performance", heightDeleteSeries, factorDeleteSeries);
     }
 
     private static long measureInsertPerformance(AssociativeContainer<Integer, String> tree, int numOperations) {
@@ -112,19 +81,17 @@ public class Main {
     }
 
     private static long measureFindPerformance(AssociativeContainer<Integer, String> tree, int numOperations) {
-        Random rand = new Random();
         long startTime = System.nanoTime();
         for (int i = 0; i < numOperations; i++) {
-            tree.find(rand.nextInt(numOperations));
+            tree.find(random.nextInt(numOperations));
         }
         return System.nanoTime() - startTime;
     }
 
     private static long measureUpdatePerformance(AssociativeContainer<Integer, String> tree, int numOperations) {
-        Random rand = new Random();
         long startTime = System.nanoTime();
         for (int i = 0; i < numOperations; i++) {
-            tree.update(rand.nextInt(numOperations), "Updated Value " + i);
+            tree.update(random.nextInt(numOperations), "Updated Value " + i);
         }
         return System.nanoTime() - startTime;
     }
@@ -137,13 +104,13 @@ public class Main {
         return System.nanoTime() - startTime;
     }
 
-    private static void displayChart(String title, String xAxisLabel, String yAxisLabel, XYSeries heightSeries, XYSeries factorSeries) {
+    private static void displayChart(String title, XYSeries heightSeries, XYSeries factorSeries) {
         XYSeriesCollection dataset = new XYSeriesCollection();
         dataset.addSeries(heightSeries);
         dataset.addSeries(factorSeries);
 
         JFreeChart chart = ChartFactory.createXYLineChart(
-                title, xAxisLabel, yAxisLabel, dataset,
+                title, "Number of Operations", "Time (ns)", dataset,
                 PlotOrientation.VERTICAL, true, true, false);
 
         ChartPanel chartPanel = new ChartPanel(chart);
